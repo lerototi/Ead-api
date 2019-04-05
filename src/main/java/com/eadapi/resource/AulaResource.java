e8483d0e1f673f75917f4ba0bce53f529825634a
Lerototi <lerototi@gmail.com>
2019-04-05 16:25:41 -0300

    correção bug

--- src/main/java/com/eadapi/resource/AulaResource.java
+++ src/main/java/com/eadapi/resource/AulaResource.java
@@ -5,10 +5,8 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -53,9 +51,9 @@ public class AulaResource {
 		return ResponseEntity.status(HttpStatus.CREATED).body(aulaSalva);
 	}
 	
-	@GetMapping("/{id_aula}")
-	public ResponseEntity<?> buscarPorId(@PathVariable Long id){
-		Aula aula = aulaRepository.findOne(id);
+	@GetMapping("/{idAula}")
+	public ResponseEntity<?> buscarPorId(@PathVariable Long idAula){
+		Aula aula = aulaRepository.findOne(idAula);
 		return aula != null ? ResponseEntity.ok(aula) : ResponseEntity.notFound().build();
 	}
 	
